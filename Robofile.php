15bdaa014ac95b43b559e64e02f701324fada2fc
Paolo Mainardi <paolo@twinbit.it>
2015-04-04 19:08:35 +0200

    refs #22: Git publish task

--- Robofile.php
+++ Robofile.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Finder\Finder as Finder;
 use Symfony\Component\Yaml\Parser as Parser;
+use Robo\Task\Development\SemVer;
 
 /**
  * This is project's console commands configuration for the Drocker project.
@@ -16,6 +17,24 @@ class Robofile extends \Robo\Tasks
       $this->yell("Releasing Spark...");
       $this->buildPhar();
       $this->buildSemver();
+      $this->buildPublish();
+    }
+
+   /**
+    * Publish the package on the release branch.
+    */
+    public function buildPublish() {
+      $semver_file = '.semver';
+      $semver = new SemVer($semver_file);
+      rename('spark.phar', 'spark-release.phar');
+      $this->taskGitStack()->checkout('release')->run();
+      rename('spark-release.phar', 'spark.phar');
+      $this->taskGitStack()
+          ->add('spark.phar')
+          ->commit('spark ' . (string) $semver . ' released')
+          ->push('origin', 'release')
+          ->checkout('develop')
+          ->run();
     }
 
     /**
