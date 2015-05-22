edc6070e550bac0684d7a8eb8e2468e9e00607c3
Giuseppe Rota <rota.giuseppe@gmail.com>
2015-05-22 11:10:22 +0200

    refs #7: helpful error message when no configuration

--- spark.php
+++ spark.php
@@ -7,8 +7,7 @@
  */
 if (strpos(basename(__FILE__), 'phar')) {
     require_once 'phar://spark.phar/vendor/autoload.php';
-}
-else{
+} else {
     if (file_exists(__DIR__.'/vendor/autoload.php')) {
         require_once __DIR__.'/vendor/autoload.php';
     } elseif (file_exists(__DIR__.'/../../autoload.php')) {
@@ -23,23 +22,25 @@ use Sparkfabrik\Tools\Spark\Command\Redmine\RedmineSearchCommand;
 use Sparkfabrik\Tools\Spark\Command\Redmine\RedmineShowCommand;
 use Sparkfabrik\Tools\Spark\Command\Redmine\RedmineGitBranchCommand;
 use Robo\Task\Development\SemVer;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
 try {
-  $semver_file = '.semver';
-  if (!file_exists($semver_file)) {
-    $semver_file = 'phar://spark.phar/.semver';
-  }
-  $semver = new SemVer($semver_file);
-  $application = new Application('Spark', (string) $semver);
-  $config = new SparkConfigurationWrapper();
-  $config->loadConfig();
+    $semver_file = '.semver';
+    if (!file_exists($semver_file)) {
+        $semver_file = 'phar://spark.phar/.semver';
+    }
+    $semver = new SemVer($semver_file);
+    $application = new Application('Spark', (string) $semver);
+    $config = new SparkConfigurationWrapper();
+    $config->loadConfig();
 
-  // Load commands.
-  $application->add(new RedmineSearchCommand);
-  $application->add(new RedmineShowCommand);
-  $application->add(new RedmineGitBranchCommand);
-  $application->run();
+    // Load commands.
+    $application->add(new RedmineSearchCommand);
+    $application->add(new RedmineShowCommand);
+    $application->add(new RedmineGitBranchCommand);
+    $application->run();
+} catch (InvalidConfigurationException $e) {
+    exit(1);
+} catch (Exception $e) {
+    print $e->getMessage() . PHP_EOL;
 }
-catch (Exception $e) {
-  print $e->getMessage() . PHP_EOL;
-}
-
