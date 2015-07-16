64adb097dec8729721dcb75b4b2f12fe0145f3f0
vincenzo.dibiaggio <vincenzo.dibiaggio@agavee.com>
2015-07-16 18:19:46 +0200

    refs #ST-4 - Gitlab client implementation

--- src/Sparkfabrik/Command/Gitlab/GitlabCommand.php
+++ src/Sparkfabrik/Command/Gitlab/GitlabCommand.php
@@ -13,6 +13,7 @@ namespace Sparkfabrik\Tools\Spark\Command\Gitlab;
 
 use Sparkfabrik\Tools\Spark\Command\SparkCommand;
 use Sparkfabrik\Tools\Spark\SparkConfigurationWrapper;
+use Sparkfabrik\Tools\Spark\Services\GitlabService;
 use Symfony\Component\Console\Command\Command;
 
 /**
@@ -24,9 +25,6 @@ use Symfony\Component\Console\Command\Command;
  */
 class GitlabCommand extends SparkCommand
 {
-  private $gitlabConfig;
-  private $gitlabClient;
-
   /**
    * Constructor.
    *
@@ -38,42 +36,14 @@ class GitlabCommand extends SparkCommand
    */
   public function __construct($name = null) {
     parent::__construct($name);
-    $this->initConfig();
   }
 
   /**
-   * Return gitlab configuration.
-   */
-  public function getGitlabConfig() {
-    return $this->gitlabConfig;
-  }
-
-  /**
-   * Return gitlab client.
-   */
-  public function getGitlabClient() {
-    $client = $this->gitlabClient;
-    if (!$client) {
-      throw new \Exception('Gitlab client not defined');
-    }
-    return $client;
-  }
-
-  /**
-   * Create gitlab client.
-   */
-  private function createGitlabClient() {
-    $this->gitlabClient = new \Gitlab\Client($this->gitlabConfig['gitlab_url']);
-    $this->gitlabClient->authenticate($this->gitlabConfig['gitlab_token'], \Gitlab\Client::AUTH_URL_TOKEN);
-  }
-
-  /**
-   * Initialize configurations and client.
-   */
-  protected function initConfig() {
-    $configManager = new SparkConfigurationWrapper();
-    $this->gitlabConfig = $configManager->getValueFromConfig('services', 'gitlab_credentials');
-    $this->gitlabConfig['project_id'] = $configManager->getValueFromConfig('projects', 'gitlab_project_id');
-    $this->createGitlabClient();
+  * Initialize configurations and client.
+  */
+  protected function initService()
+  {
+      $this->service = new GitlabService();
+      $this->service->run();
   }
 }
---
+++ src/Sparkfabrik/Services/GitlabService.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Spark tool package.
+ *
+ * (c) Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sparkfabrik\Tools\Spark\Services;
+
+use Sparkfabrik\Tools\Spark\Services\AbstractService;
+use Sparkfabrik\Tools\Spark\Services\ServiceInterface;
+use Sparkfabrik\Tools\Spark\SparkConfigurationWrapper;
+use Sparkfabrik\Tools\Spark\SparkConfigurationWrapperInterface;
+use Gitlab\Client;
+
+/**
+ * Base class for all redmine commands.
+ *
+ * @author Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
+ *
+ * @api
+ */
+class GitlabService extends AbstractService
+{
+    protected function initConfig(SparkConfigurationWrapperInterface $config = null)
+    {
+        if (empty($config)) {
+            $config = new SparkConfigurationWrapper();
+        }
+        $this->config = $config->getValueFromConfig('services', 'gitlab_credentials');
+        $this->config['project_id'] = $config->getValueFromConfig('projects', 'gitlab_project_id');
+    }
+
+    protected function initClient()
+    {
+        if (empty($this->client)) {
+            $this->client = new \Gitlab\Client(
+                $this->config['gitlab_url']
+            );
+            $this->client->authenticate($this->config['gitlab_token'], \Gitlab\Client::AUTH_URL_TOKEN);
+            if (empty($this->client)) {
+                throw new \Exception('Cannot connect to redmine client, check your configurations.');
+            }
+        }
+    }
+}
